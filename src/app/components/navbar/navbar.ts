6b67004eb7e2e9ce144ceb2fa4b55ce7f901bd73
dandominicstaicu <dando.ds11@gmail.com>
2025-10-18 21:06:13 +0300

    navbar icons

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,25 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
-export class NavbarComponent {
-  onHomeClick() {
-    console.log('Home clicked');
-  }
-
-  onNotificationsClick() {
-    console.log('Notifications clicked');
-  }
-
-  onMapClick() {
-    console.log('Map clicked');
-  }
-
-  onProfileClick() {
-    console.log('Profile clicked');
-  }
-}
+export class NavbarComponent {}
